cdc1491d4ecb1189a1c0e60bd9f9b1176679cbb4
Turas <182771626+Turas39@users.noreply.github.com>
2026-03-19 19:30:02 +0100

    add Model

--- FleetManager/Models/Vehicle.cs
+++ FleetManager/Models/Vehicle.cs
@@ -1,6 +1,35 @@
+using ReactiveUI;
+
 namespace FleetManager.Models;
 
-public class Vehicle
+public class Vehicle : ReactiveObject
 {
+    private string _name;
+    private string _licenseNumber;
+    private int _fuelPercentage;
+    private string _status;
+
+    public string Name
+    {
+        get => _name;
+        set => this.RaiseAndSetIfChanged(ref _name, value);
+    }
     
+    public string LicenseNumber
+    {
+        get => _licenseNumber;
+        set => this.RaiseAndSetIfChanged(ref _licenseNumber, value);
+    }
+    
+    public int FuelPercentage
+    {
+        get => _fuelPercentage;
+        set => this.RaiseAndSetIfChanged(ref _fuelPercentage, value);
+    }
+    
+    public string Status
+    {
+        get => _status;
+        set => this.RaiseAndSetIfChanged(ref _status, value);
+    }
 }
